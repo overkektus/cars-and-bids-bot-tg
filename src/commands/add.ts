3cfdacf903f4d13f108c851f6c71af78f07a4869
Yahor Piskunou <egor.piskunou@gmail.com>
2023-02-05 15:29:02 +0100

    feat: add dublicates validation befor adding new car

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -15,15 +15,23 @@ export class AddCommand extends Command {
   }
 
   async addNewCarConversation(conversation: BotConversation, ctx: BotContext): Promise<void> {
+    const userId = ctx.from?.id;
+    let isDublicate: boolean = false;
+    let carURL: string = '';
+
     await ctx.reply('Send, please, URL of car.');
-    const carURL = (await conversation.wait()).message?.text;
-    // TODO: add URL validation
-    // TODO: check for dublicates
-    if (carURL) {
-      const carTitle = await this.grabCarTitle(carURL);
-      await carModel.create({ url: carURL, userId: ctx.from?.id, carTitle });
-      ctx.reply(`${carTitle} was succesfully added to list.✅`);
-    }
+    do {
+      carURL = (await conversation.wait()).message?.text!;
+      // TODO: add URL validation
+      isDublicate = !!await carModel.count({ url: carURL, userId });
+      if (isDublicate) {
+        await ctx.reply('Already exist. Send another URL');
+      }
+    } while(isDublicate);
+
+    const carTitle = await this.grabCarTitle(carURL);
+    await carModel.create({ url: carURL, userId, carTitle });
+    ctx.reply(`${carTitle} was succesfully added to list.✅`);
   }
 
   private async grabCarTitle(carURL: string): Promise<string> {
