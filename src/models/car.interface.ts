2a15653c72a5b29a40587440d5fcb78726f9b648
Yahor Piskunou <egor.piskunou@gmail.com>
2023-02-23 22:41:27 +0100

    update car model

--- src/models/car.interface.ts
+++ src/models/car.interface.ts
@@ -1,5 +1,23 @@
+type EventType = 'bid' | 'comment';
+
+interface BaseEvent {
+  id: string;
+  type: EventType;
+}
+
+interface BidEvent extends BaseEvent {
+  value: number;
+}
+
+interface CommentEvent extends BaseEvent {
+  comment: string;
+}
+
+export type ThreadEvent = CommentEvent | BidEvent;
+
 export interface ICar {
   userId: number;
   url: string;
   carTitle: string;
+  events: Array<ThreadEvent>
 }
--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -1,11 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
-import { ICar } from "./car.interface";
+import { ICar, ThreadEvent } from "./car.interface";
 
 const CarSchema: Schema = new Schema({
   url: { type: String, required: true },
   userId: { type: Number, require: true },
-  carTitle: { type: String, require: true }
+  carTitle: { type: String, require: true },
+  events: { type: Array<ThreadEvent> }
 });
 
 export default mongoose.model<ICar>('Car', CarSchema);
